0c171e3755099abfd87a04cee204e019484c9ca7
Arpaka <git@arpaka.com>
2017-07-26 03:56:22 +0900

    add test_sigmoid.cc

---
+++ ch03/test/test_sigmoid.cc
@@ -0,0 +1,21 @@
+#include "types.h"
+#include "sigmoid.h"
+#include "matplotlibcpp.h"
+//#include "xtensor/xio.hpp"
+//#include <iostream>
+
+namespace plt = matplotlibcpp;
+
+int main() {
+  array_t<double> x = xt::arange<double>(-5.0, 5.0, 0.1);
+  array_t<double> y = sigmoid(x);
+  //std::cout << y << std::endl;
+
+  auto vx = arr_to_vec(x);
+  auto vy = arr_to_vec(y);
+  plt::plot(vx, vy);
+  plt::ylim(-0.1, 1.1);
+  plt::show();
+
+  return 0;
+}
