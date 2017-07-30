4629a6d4d5068939ac99ea489e14894988368a47
Arpaka <git@arpaka.com>
2017-07-30 21:12:55 +0900

    refactor

--- ch04/test/test_gradient_2d.cc
+++ ch04/test/test_gradient_2d.cc
@@ -1,7 +1,4 @@
 #include "types.hpp"
-#include "xtensor/xbuilder.hpp"
-#include "xtensor/xarray.hpp"
-#include "xtensor/xslice.hpp"
 #include "xtensor/xio.hpp"
 #include "xt_helper.hpp"
 #include <iostream>
@@ -42,10 +39,9 @@ T numerical_gradient(func_t<T> f, T x)
   } else {
     auto grad = x;
     for (uint idx=0; idx<x.shape()[0]; ++idx) {
+      // attention: often mistakes x[idx] ...
       auto xx = xt::view(x, idx);
-      //std::cout << xx << std::endl;
       xt::view(grad, idx) = numerical_gradient_no_batch<T>(f, xx);
-      //std::cout << xt::view(grad, idx) << std::endl;
     }
     return grad;
   }
@@ -65,25 +61,15 @@ int main()
 {
   array_t<double> x0 = xt::arange(-2.0, 2.5, 0.25);
   array_t<double> x1 = xt::arange(-2.0, 2.5, 0.25);
-  //std::cout << x0 << std::endl; // -2, -1.75...
-  //std::cout << x1 << std::endl; // -2, -1.75...
   auto mz = xt::meshgrid(x0, x1);
   auto my = std::get<0>(mz);
   auto mx = std::get<1>(mz);
-  //std::cout << mx << std::endl; // [-2,-1.75...], [-2,-1.75...]...
-  //std::cout << my << std::endl; // [-2,-2...], [-1.75,-1.75...]...
   auto fx = xt::flatten<array_t<double>>(mx);
   auto fy = xt::flatten<array_t<double>>(my);
-  //std::cout << fx << std::endl; // [-2,-1.75...-2,-1.75...]
-  //std::cout << fy << std::endl; // [-2,-2...-1.75,-1.75...]
 
   //auto gx = numerical_gradient<array_t<double>>(function_2<array_t<double>>, fx);
   //auto gy = numerical_gradient<array_t<double>>(function_2<array_t<double>>, fy);
-  //std::cout << gx << std::endl;
-  //std::cout << gy << std::endl;
-
   array_t<double> fz = xt::stack(xt::xtuple(fx, fy), 0);
-  //std::cout << fz << std::endl;
   auto grad = numerical_gradient<array_t<double>>(function_2<array_t<double>>, fz);
   std::cout << grad << std::endl;
 
